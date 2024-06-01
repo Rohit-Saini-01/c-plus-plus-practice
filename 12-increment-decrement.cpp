22351f195f0d24c93fb9cbed033538d5cebbf143
Rohit Saini <emailrohitsaini@gmail.com>
2024-06-01 19:18:05 +0530

    Increment & decrement

---
+++ 12-increment-decrement.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    int num1;
+    std::cout << "Enter a number you want to experiment increment decrement operator on\n";
+    std::cin >> num1;
+    std::cout << "num1++\n" << num1++<<"\n";
+    std::cout << "num1--\n" << num1--<<"\n";
+    std::cout << "++num1\n" << ++num1<<"\n";
+    std::cout << "--num1\n" << --num1<<"\n";
+    return 0;
+}
