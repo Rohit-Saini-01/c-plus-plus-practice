330a0a0affe40d88bdbfcb1f1e794afb38f532f4
Rohit Saini <emailrohitsaini@gmail.com>
2024-06-01 18:18:32 +0530

    Compound assignment

--- 11-compound-assignment.cpp
+++ 11-compound-assignment.cpp
@@ -3,14 +3,14 @@
 int main()
 {
     int num1, num2;
-    std::cout << "ENter two numbers\n";
+    std::cout << "Enter two numbers\n";
     std::cin >> num1 >> num2;
     num1 += num2;
-    std::cout << num1<<"\n";
+    std::cout << num1 << "\n";
     num1 *= num2;
-    std::cout << num1<<"\n";
+    std::cout << num1 << "\n";
     num1 -= num2;
-    std::cout << num1<<"\n";
+    std::cout << num1 << "\n";
     num1 /= num2;
-    std::cout << num1<<"\n";
+    std::cout << num1 << "\n";
 }
