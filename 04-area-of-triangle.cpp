f556111a5120ac40734b66b121b6839e58988412
Rohit Saini <emailrohitsaini@gmail.com>
2024-06-01 15:21:56 +0530

    Area of triangle

---
+++ 04-area-of-triangle.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+int main(){
+    std::cout<<"Enter the base and height of the triangle\n";
+    float base, height, area;
+    std::cin>>base>>height;
+    area = (float)1/2*base*height;
+    std::cout<<"Area of the triangle is "<<area;
+    return 0;
+}
