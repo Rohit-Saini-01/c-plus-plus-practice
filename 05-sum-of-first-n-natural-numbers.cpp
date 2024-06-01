e923b89a253da7c5ec2e7b380055e48759b8bd7e
Rohit Saini <emailrohitsaini@gmail.com>
2024-06-01 15:27:20 +0530

    Sum of first n natural numbers

---
+++ 05-sum-of-first-n-natural-numbers.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+int main()
+{
+    int n,sum;
+    std::cout<<"Enter the value of n\n";
+    std::cin>>n;
+    sum = n*(n+1)/2;
+    std::cout<<"Sum of first "<<n<<" natural numbers is "<<sum;
+    return 0;
+}
