b8b3a2bc870ea5ea71ca5e3c8df735b8a2d87c6e
Rohit Saini <emailrohitsaini@gmail.com>
2024-06-11 21:28:47 +0530

    roots of a quadratic equation

---
+++ 06-root-of-quadratic-equation.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <math.h>
+
+int main()
+{
+    float a, b, c, root1, root2;
+    std::cout << "Enter the value of coefficients a,b and c \n";
+    std::cin >> a >> b >> c;
+    root1 = (-b + sqrt(b * b - 4 * a * c)) / (2 * a);
+    root2 = (-b - sqrt(b * b - 4 * a * c)) / (2 * a);
+    std::cout << "Roots are " << root1 <<" and "<< root2;
+    return 0;
+}
+
+// give me python equivalent code for this C++ code
