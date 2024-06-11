05c64dbc68a465cbbacb39bb9c5e12e76f0e10aa
Rohit Saini <emailrohitsaini@gmail.com>
2024-06-11 20:54:56 +0530

    sum of first n natural numbers

---
+++ 05-sum-of-first-n-natural-numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    int num, sum;
+    std::cout << "Enter the value of n \n";
+    std::cin >> num;
+    sum = (num * (num + 1)) / 2;
+    std::cout << "Sum of first " << num << " natural numbers is " << sum;
+    return 0;
+}
+
+// give me python equivalent code for this C++ code
