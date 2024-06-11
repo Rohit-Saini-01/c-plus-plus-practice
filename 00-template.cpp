df0569cf913f7fd25f4134260bca3522ba1ca8ce
Rohit Saini <emailrohitsaini@gmail.com>
2024-06-11 20:38:32 +0530

    area of a triangle

--- 00-template.cpp
+++ 00-template.cpp
@@ -5,3 +5,5 @@ int main()
 
     return 0;
 }
+
+// give me python equivalent code for this C++ code
---
+++ 04-area-of-triangle.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    float base, height, area;
+    std::cout << "Enter the base and height of the triangle \n";
+    std::cin >> base >> height;
+    area = (base * height) / 2;
+    std::cout << "Area of the triangle is " << area;
+    return 0;
+}
+
+// give me python equivalent code for this C++ code
