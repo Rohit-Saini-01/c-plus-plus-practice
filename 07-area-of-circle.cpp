1ceca65d1941c78cc961e37acc803b2de5b07ced
Rohit Saini <emailrohitsaini@gmail.com>
2024-06-11 21:42:48 +0530

    area of circle

---
+++ 07-area-of-circle.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    float radius, area;
+    std::cout << "Enter the value of radius \n";
+    std::cin >> radius;
+    area = 3.14 * radius * radius;
+    std::cout << "Area of the circle with radius " << radius << " is " << area;
+    return 0;
+}
