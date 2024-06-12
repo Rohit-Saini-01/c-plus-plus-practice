31a8329f877f6048738e68446afeb9d05b49ea8b
Rohit Saini <emailrohitsaini@gmail.com>
2024-06-12 17:25:56 +0530

    nested if else

---
+++ 22-nested-if-else.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main()
+{
+    float num1, num2, num3;
+
+    std::cout << "Enter three numbers\n";
+    std::cin >> num1 >> num2 >> num3;
+
+    if (num1 > num2 && num1 > num3)
+        std::cout << num1;
+    else
+    {
+        if (num2 > num3)
+            std::cout << num2;
+        else
+            std::cout << num3;
+    }
+
+    return 0;
+}
+
+// give me python equivalent code for this C++ code
