ded3a4e63a2b67cad3bdc92920f0d4bcb365435c
Rohit Saini <emailrohitsaini@gmail.com>
2024-06-12 16:54:24 +0530

    compound conditional statement

---
+++ 20-compound-conditional-statement.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+    float hour;
+
+    std::cout << "Enter hour\n";
+    std::cin >> hour;
+
+    if (hour >= 9 && hour <= 18)
+        std::cout << "Working Hour\n";
+    else
+        std::cout << "Leisure Hour\n";
+
+    return 0;
+}
+
+// give me python equivalent code for this C++ code
