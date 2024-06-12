f938b8f01654881b66799dd3755c8aa75667b448
Rohit Saini <emailrohitsaini@gmail.com>
2024-06-12 14:44:30 +0530

    max of two numbers

---
+++ 17-max-of-two-numbers.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main()
+{
+    float num1, num2;
+
+    std::cout << "Enter two numbers\n";
+
+    std::cin >> num1 >> num2;
+
+    if (num1 > num2)
+        std::cout << "Max is " << num1;
+    else
+        std::cout << "Max is " << num2;
+
+    return 0;
+}
+
+// give me python equivalent code for this C++ code
