beec0a4e5d16839f19d8b3285afab7565886ed15
Rohit Saini <emailrohitsaini@gmail.com>
2024-06-12 06:08:21 +0530

    increment-decrement

---
+++ 13-increment-decrement.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    int x = 10;
+    std::cout << "x++:" << x++<<"\n";
+    std::cout << "++x:" << ++x<<"\n";
+    std::cout << "x--:" << x--<<"\n";
+    std::cout << "--x:" << --x<<"\n";
+    return 0;
+}
+
+// give me python equivalent code for this C++ code
