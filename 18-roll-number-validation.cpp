d885afb8698a959ab733526f7879b7d695d3ff09
Rohit Saini <emailrohitsaini@gmail.com>
2024-06-12 15:26:37 +0530

    roll number validation

---
+++ 18-roll-number-validation.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+    int roll_number;
+
+    std::cout << "Enter your Roll_Number\n";
+    std::cin >> roll_number;
+
+    if (roll_number < 1)
+        std::cout << "Invalid Roll Number\n";
+    else
+        std::cout << "Valid Roll Number\n";
+
+    return 0;
+}
+
+// give me python equivalent code for this C++ code
