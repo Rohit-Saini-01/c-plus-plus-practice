55f5ff97a3143d9396f922b69d229ff575310363
Rohit Saini <emailrohitsaini@gmail.com>
2024-06-12 15:39:11 +0530

    division validation

---
+++ 19-division-validaation.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+    float denominator, numerator, result;
+
+    std::cout << "Enter the value of numerator and denominator\n";
+    std::cin >> numerator >> denominator;
+
+    if (denominator == 0)
+        std::cout << "Divison by 0\n";
+    else
+        std::cout << numerator / denominator;
+
+    return 0;
+}
+
+// give me python equivalent code for this C++ code
