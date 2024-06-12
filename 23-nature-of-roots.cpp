44b82e0b0ba92fa20c2d90995665298306facd39
Rohit Saini <emailrohitsaini@gmail.com>
2024-06-12 19:31:23 +0530

    nature of quadratic roots

---
+++ 23-nature-of-roots.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <math.h>
+
+int main()
+{
+    float a, b, c, discriminant, root1, root2;
+
+    std::cout << "Enter the value of coefficient a,b,c\n";
+    std::cin >> a >> b >> c;
+
+    discriminant = b * b - 4 * a * c;
+
+    if (discriminant == 0)
+    {
+        root1 = root2 = -b / (2 * a);
+        std::cout << "Real and equal roots " << root1;
+    }
+    else if (discriminant > 0)
+    {
+        root1 = (-b + std::sqrt(discriminant)) / (2 * a);
+        root2 = (-b - std::sqrt(discriminant)) / (2 * a);
+        std::cout << "Real and unequal roots \n"
+                  << root1 << " and " << root2;
+    }
+    else
+        std::cout << "Imaginary roots\n";
+
+    return 0;
+}
+
+// give me python equivalent code for this C++ code
