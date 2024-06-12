fdeea817af7197f5e9da018b1aad62f425583c0f
Rohit Saini <emailrohitsaini@gmail.com>
2024-06-12 05:56:16 +0530

    compound assignment

---
+++ 12-compound-assignment.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    int x = 10;
+    x = x + 1;
+    x += 1;
+    std::cout << "X:" << x;
+    return 0;
+}
+
+// give me python equivalent code for this C++ code
