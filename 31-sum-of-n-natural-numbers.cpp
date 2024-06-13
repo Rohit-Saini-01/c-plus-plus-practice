897f0d87a41491186a9a2a9d249a8ae107a78ffa
Rohit Saini <emailrohitsaini@gmail.com>
2024-06-13 09:07:32 +0530

    sum of n natural numbers

---
+++ 31-sum-of-n-natural-numbers.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main()
+{
+    int num, sum = 0;
+
+    std::cout << "Enter the value of n:\n";
+    std::cin >> num;
+
+    for (int i = 1; i <= num; i++)
+    {
+        sum += i;
+    }
+    std::cout << sum;
+
+    return 0;
+}
+
+// give me python equivalent code for this C++ code
