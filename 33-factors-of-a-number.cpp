a0566392ba137d6d0db6bb52e2cc90bc75c19c16
Rohit Saini <emailrohitsaini@gmail.com>
2024-06-13 10:08:19 +0530

    factors of a number

---
+++ 33-factors-of-a-number.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main()
+{
+    int num;
+
+    std::cout << "Enter the value of num\n";
+    std::cin >> num;
+
+    for (int i = 1; i <= num; i++)
+    {
+        if (num % i == 0)
+            std::cout << i << "\n";
+    }
+
+    return 0;
+}
+
+// give me python equivalent code for this C++ code
