827eb2c54e321dbbcb9b14d38ca041e8137d0ebe
Rohit Saini <emailrohitsaini@gmail.com>
2024-06-13 07:41:28 +0530

    short-circuit

---
+++ 26-short-circuit.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main()
+{
+    int var1=5, var2=3,i=1;
+
+    if(var1<var2 && ++i<var2)
+    {
+
+    }
+    std::cout<<i<<"\n";
+
+    if(var1>var2 || ++i<var2)
+    {
+
+    }
+    std::cout<<i;
+
+    return 0;
+}
+
+// give me python equivalent code for this C++ code
