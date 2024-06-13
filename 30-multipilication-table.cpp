892452d9bba32c329a2ce64922b345fd3054e20b
Rohit Saini <emailrohitsaini@gmail.com>
2024-06-13 08:58:08 +0530

    multiplication table

---
+++ 30-multipilication-table.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+    int num;
+
+    std::cout << "Enter the value of n:\n";
+    std::cin >> num;
+
+    for (int i = 1; i <= 10; i++)
+        std::cout << num << " X " << i << " = " << num * i << "\n";
+
+    return 0;
+}
+
+// give me python equivalent code for this C++ code
