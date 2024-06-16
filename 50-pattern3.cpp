f13e150184a18bda3f5e218e93346db582a16013
Rohit Saini <emailrohitsaini@gmail.com>
2024-06-16 13:02:58 +0530

    pattern 3

---
+++ 50-pattern3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main()
+{
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            if (i > j)
+            {
+                std::cout << " ";
+            }
+            else
+            {
+                std::cout << "*";
+            }
+        }
+        std::cout<<"\n";
+    }
+    return 0;
+}
