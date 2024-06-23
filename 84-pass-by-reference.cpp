f72ea7476bef87d00892ed199e0706fa46ee3764
Rohit Saini <emailrohitsaini@gmail.com>
2024-06-23 18:19:14 +0530

    pass by reference

---
+++ 84-pass-by-reference.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+int swap(int &var_1, int &var_2)
+{
+    int temp = var_1;
+    var_1 = var_2;
+    var_2 = temp;
+
+    return 0;
+}
+
+int main()
+{
+    int var_1 = 10, var_2 = 20;
+    swap(var_1, var_2);
+
+    std::cout << var_1 << " " << var_2 << "\n";
+
+    return 0;
+}
