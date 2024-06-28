552f0a331baa894bc08b817f18c2b8fb0c157f9a
Rohit Saini <emailrohitsaini@gmail.com>
2024-06-28 18:36:22 +0530

    virtual functions

---
+++ 101-virtual-functions.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+class Parent
+{
+public:
+    //virtual function
+   virtual int display()
+    {
+        std::cout << "Display of parent\n";
+        return 0;
+    }
+};
+
+class Child : public Parent
+{
+public:
+    int display()
+    {
+        std::cout << "Display of child\n";
+        return 0;
+    }
+};
+
+int main()
+{
+
+    Parent *pr = new Child;//base class pointer derived class object
+
+    pr->display(); // child class function will be called
+
+    return 0;
+}
